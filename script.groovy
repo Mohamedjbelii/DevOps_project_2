4e580e05a79016848f6c83b6fb224eec7a10b7c1
Mohamedjbelii <mohamedjbeli26@gmail.com>
2024-07-10 00:00:19 +0100

    changing app version

--- script.groovy
+++ script.groovy
@@ -10,7 +10,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dd832e86-f6fd-43bf-bb76-aa9f96fe2cd5', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t  mohamedjbelii/devops_bootcamp:jma-2.0 .'
+        sh 'docker build -t  mohamedjbelii/devops_bootcamp:jma-4.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push mohamedjbelii/devops_bootcamp:jma-2.0'
     }
