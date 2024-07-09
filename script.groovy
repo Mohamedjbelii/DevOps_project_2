244e8fce71c370c7662fcca9a6babafeda9ecc37
Mohamedjbelii <mohamedjbeli26@gmail.com>
2024-07-10 00:25:13 +0100

    correcting the typo error

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ def incrementJar() {
     echo "incrementing the jar file..."
     sh 'mvn build-helper:parse-version versions:set \
         -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
-         version:commit'
+         versions:commit'
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
     def version = matcher[0][1]
     env.IMAGE_NAME = "$version-$BUILD_NUMBER"
